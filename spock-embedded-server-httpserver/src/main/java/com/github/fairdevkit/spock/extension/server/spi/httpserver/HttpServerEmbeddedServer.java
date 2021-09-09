f0399370c5f335969a0dff703dd1416812d62441
kburger <burger.github@gmail.com>
2021-09-09 15:37:48 +0200

    Added com.sun.net.httpserver implementation of the extension's SPI.

---
+++ spock-embedded-server-httpserver/src/main/java/com/github/fairdevkit/spock/extension/server/spi/httpserver/HttpServerEmbeddedServer.java
@@ -0,0 +1,92 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 fairdevkit
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.github.fairdevkit.spock.extension.server.spi.httpserver;
+
+import com.github.fairdevkit.spock.extension.server.spi.EmbeddedServer;
+import com.github.fairdevkit.spock.extension.server.spi.ResourceContext;
+import com.sun.net.httpserver.HttpServer;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class HttpServerEmbeddedServer implements EmbeddedServer {
+    private final AtomicInteger refCount;
+    private HttpServer server;
+
+    public HttpServerEmbeddedServer() {
+        refCount = new AtomicInteger();
+    }
+
+    @Override
+    public InetSocketAddress start() throws IOException {
+        return start(0);
+    }
+
+    @Override
+    public InetSocketAddress start(int port) throws IOException {
+        if (refCount.get() == 0) {
+            server = HttpServer.create(new InetSocketAddress(port), 0);
+            server.start();
+        }
+
+        refCount.incrementAndGet();
+
+        return server.getAddress();
+    }
+
+    @Override
+    public void createContext(ResourceContext ctx) {
+        server.createContext(ctx.path(), exchange -> {
+            var headers = exchange.getResponseHeaders();
+            headers.add("Content-Type", ctx.contentType());
+
+            var stream = new ByteArrayOutputStream();
+            var length = getClass().getResourceAsStream(ctx.resource()).transferTo(stream);
+
+            exchange.sendResponseHeaders(ctx.status(), length);
+
+            try (var output = exchange.getResponseBody()) {
+                stream.writeTo(output);
+            }
+        });
+    }
+
+    @Override
+    public void removeContext(ResourceContext ctx) {
+        server.removeContext(ctx.path());
+    }
+
+    @Override
+    public void stop() {
+        if (refCount.get() <= 0) {
+            throw new IllegalStateException();
+        }
+
+        var count = refCount.decrementAndGet();
+        if (count == 0) {
+            server.stop(0);
+        }
+    }
+}
